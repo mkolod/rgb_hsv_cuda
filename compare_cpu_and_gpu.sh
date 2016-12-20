121d7cd35817f0769bc9105e58fed55fa3438b9a
Marek Kolodziej <mkolod@gmail.com>
2016-12-19 17:03:44 -0800

    O3->O2 to prevent incorrect dead code elimination

--- compare_cpu_and_gpu.sh
+++ compare_cpu_and_gpu.sh
@@ -1,8 +1,8 @@
 export CFLAGS="-O3 -march=native -mtune=native -m64 -ffast_math"
 
-nvcc -O3 -c -gencode arch=compute_61,code=sm_61 -gencode arch=compute_61,code=compute_61 gpu_impl.cu -o gpu_impl.o
+nvcc -O2 -c -gencode arch=compute_61,code=sm_61 -gencode arch=compute_61,code=compute_61 gpu_impl.cu -o gpu_impl.o
 
-nvcc -O3 -arch=sm_61 cpu_impl.cu gpu_impl.o -o cpu_impl `pkg-config opencv --cflags --libs`
+nvcc -O2 -arch=sm_61 cpu_impl.cu gpu_impl.o -o cpu_impl `pkg-config opencv --cflags --libs`
 
 ./cpu_impl
 
